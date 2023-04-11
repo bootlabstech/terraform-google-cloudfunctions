c32052918a43b3f52f362fcd5cddbba137a9e43f
Silpashree Acharya <silpashreeacharya@Silpashrees-MacBook-Air.local>
2023-04-11 11:39:47 +0530

    rem other res

--- main.tf
+++ main.tf
@@ -1,42 +1,41 @@
-resource "google_project_service" "funcapi" {
-  project = var.project_id
-  service = "cloudfunctions.googleapis.com"
-}
+# resource "google_project_service" "funcapi" {
+#   project = var.project_id
+#   service = "cloudfunctions.googleapis.com"
+# }
 # resource "google_project_iam_binding" "cloud_functions_service_agent_binding" {
 #   depends_on =[google_project_service.funcapi]
-#   project = var.project_id
+#   project = var.host_project_id
 #   role    = "roles/vpcaccess.user"
-
 
 #   members = var.members
 # }
 
-resource "google_storage_bucket" "bucket" {
-  depends_on    = [data.archive_file.source]
-  project       = var.project_id
-  name          = var.bucket_name
-  location      = var.region
-  force_destroy = true
+# resource "google_storage_bucket" "bucket" {
+#   depends_on    = [data.archive_file.source]
+#   project       = var.project_id
+#   name          = var.bucket_name
+#   location      = var.region
+#   force_destroy = true
 
-  uniform_bucket_level_access = true
+#   uniform_bucket_level_access = true
 
-}
-resource "google_storage_bucket_object" "archive" {
-  depends_on = [google_storage_bucket.bucket]
-  name   = data.archive_file.source.output_path
-  bucket = google_storage_bucket.bucket.name
+# }
+# resource "google_storage_bucket_object" "archive" {
+#   depends_on = [google_storage_bucket.bucket]
+#   name   = data.archive_file.source.output_path
+#   bucket = google_storage_bucket.bucket.name
   
-  # Set the content of the zip file to the contents of the local directory
-  source = data.archive_file.source.output_path
-}
+#   # Set the content of the zip file to the contents of the local directory
+#   source = data.archive_file.source.output_path
+# }
 
 
 
-data "archive_file" "source" {
-    type        = "zip"
-    source_dir  = "function-code"
-    output_path = "tmp/function.zip"
-}
+# data "archive_file" "source" {
+#     type        = "zip"
+#     source_dir  = "function-code"
+#     output_path = "tmp/function.zip"
+# }
 
 # Add source code zip to the Cloud Function's bucket
 
@@ -51,20 +50,16 @@ resource "google_cloudfunctions_function" "function" {
     vpc_connector_egress_settings = "PRIVATE_RANGES_ONLY"
 
     # Get the source code of the cloud function as a Zip compression
-    source_archive_bucket = google_storage_bucket_object.archive.bucket
-    source_archive_object = google_storage_bucket_object.archive.name
+    source_archive_bucket = var.archive_bucket
+    source_archive_object = var.archive_object
 
     # Must match the function name in the cloud function `main.py` source code
-    entry_point           = "helloWorld"
+    entry_point           = var.entry_point
     trigger_http          = true
     
 
     # Dependencies are automatically inferred so these lines can be deleted
-    depends_on            = [
-       // google_storage_bucket.bucket,
-        google_storage_bucket_object.archive,
-        google_project_service.funcapi
-    ]
+
 }
 
 
--- variables.tf
+++ variables.tf
@@ -1,29 +1,29 @@
 variable "project_id" {
  type = string
 }
-variable "host_project_id" {
- type = string
-}
+# variable "host_project_id" {
+#  type = string
+# }
 variable "region" {
   type = string
 }
-variable "bucket_name" {
-  type = string
-}
+
 variable "vpc_connector" {
   type = string
 }
-variable "outputpath" {
-  type = string
-  default = "/tmp/function.zip"
-}
+
 variable "function_name" {
   type = string
-  default = "function-trigger-on-gcs"
 }
 variable "function_runtime" {
   type = string
 }
 variable "entry_point" {
+  type = string
+}
+variable "archive_bucket" {
+  type = string
+}
+variable "archive_object" {
   type = string
 }
